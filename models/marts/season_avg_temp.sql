7a904be7fdde8642a9aa48913b944c2a64aac4ed
franziska1403 <franziska1403@icloud.com>
2024-01-18 16:33:56 +0100

    no is in the code

--- models/marts/season_avg_temp.sql
+++ models/marts/season_avg_temp.sql
@@ -28,15 +28,15 @@ temp_evaluation as (
     SELECT
         avgtemp_c,
         case 
-            when avgtemp_c is < 0 then 'freezing'
-            when avgtemp_c is between 0.001 and 5 then 'very cold'
-            when avgtemp_c is between 5.1 and 12 then 'cold'
-            when avgtemp_c is between 12.1 and 17 then 'a little cold'
-            when avgtemp_c is between 17.1 and 22 then 'warm'
-            when avgtemp_c is between 22.1 and 30 then 'very warm'
-            when avgtemp_c is between 30.1 and 35 then 'hot'
-            when avgtemp_c is between 30.1 and 35 then 'very hot'
-            when avgtemp_c is > 35 then 'too hot'
+            when avgtemp_c < 0 then 'freezing'
+            when avgtemp_c between 0.001 and 5 then 'very cold'
+            when avgtemp_c between 5.1 and 12 then 'cold'
+            when avgtemp_c between 12.1 and 17 then 'a little cold'
+            when avgtemp_c between 17.1 and 22 then 'warm'
+            when avgtemp_c between 22.1 and 30 then 'very warm'
+            when avgtemp_c between 30.1 and 35 then 'hot'
+            when avgtemp_c between 30.1 and 35 then 'very hot'
+            when avgtemp_c > 35 then 'too hot'
             else 'not able to measure'
         end as temperature_feeling
     from {{(ref('prep_temp'))}}
