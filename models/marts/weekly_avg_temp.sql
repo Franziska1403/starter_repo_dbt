d9c73751d87934cbd4f45d43aab34aacaa4f5041
franziska1403 <franziska1403@icloud.com>
2024-01-18 11:29:52 +0100

    training

--- models/marts/weekly_avg_temp.sql
+++ models/marts/weekly_avg_temp.sql
@@ -4,7 +4,7 @@ with total_avg as(
     avg(avgtemp_c) as avg_temp_week, 
     max(maxtemp_c) as max_temp_week, 
     min(mintemp_c) as min_temp_weekday
-    from {(ref('prep_temp'))}
+    from {{(ref('prep_temp'))}}
     group by city, country, year, week, lat, lon
 )
 select * from total_avg
